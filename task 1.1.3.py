769b1b2af56c8881bc960ff2c3294b326a1defd5
TaurusK <ilyakazim2@gmail.com>
2024-11-08 11:49:01 +0300

    №3. пока не поулчается. продолжу дома

---
+++ task 1.1.3.py
@@ -0,0 +1,5 @@
+# Start learn python code to 100 tasks https://code.mu/ru/python/tasker/stager/
+# Задача 1.1 -№3  Дана строка. Выведите в консоль последний символ строки.
+
+n = str(input())
+print(str([str.length - 1]))
