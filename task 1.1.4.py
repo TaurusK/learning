9bba003eae4737d05278aa8a9dba2e4a8197a676
TaurusK <ilyakazim2@gmail.com>
2024-11-08 13:33:20 +0300

    task is done. I made it on my own

---
+++ task 1.1.4.py
@@ -0,0 +1,10 @@
+# Start learn python code to 100 tasks https://code.mu/ru/python/tasker/stager/
+# Задача 1.1 №4  Дано число. Проверьте, четное оно или нет.
+
+x = int(input('ведите число 1'))
+
+z: int = x/2
+if z == int(z):
+    print('четное')
+else:
+    print('нечетное')
