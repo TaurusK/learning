6b236e75f24fa84b4d93f97074d41660c75a6675
TaurusK <ilyakazim2@gmail.com>
2024-11-08 13:11:03 +0300

    разобрался с задачей №3 и выводом последнего символа

--- task 1.1.3.py
+++ task 1.1.3.py
@@ -1,5 +1,6 @@
 # Start learn python code to 100 tasks https://code.mu/ru/python/tasker/stager/
 # Задача 1.1 -№3  Дана строка. Выведите в консоль последний символ строки.
 
-n = str(input())
-print(str([str.length - 1]))
+n = str(input()) # создал строку ввода консоли
+last_n = n[-1]   # n[-1] - возварщает последний символ строки
+print(last_n)
