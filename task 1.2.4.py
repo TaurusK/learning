877c99b21a8b1cba5e539a27a0ba917e6d99d58e
TaurusK <ilyakazim2@gmail.com>
2024-11-11 10:42:58 +0300

    перерыв на работу

---
+++ task 1.2.4.py
@@ -0,0 +1,19 @@
+# Start learn python code to 100 tasks https://code.mu/ru/python/tasker/stager/
+# Задача 1.2 №4 Дано число. Выведите количество цифр в этом числе.
+""" задача не завершена. делаю на основепредыщего материала"""
+
+"""number3 = str(input('введите число:')) # по аналогии сделано сумма первого и последнего цифры из числа
+
+z = number3[-1].isdigit()
+k = number3[0].isdigit()
+
+if z and k:
+    digit4 = int(number3[0])
+    digit3 = int(number3[-1])
+
+print(digit3 + digit4)
+
+
+x = 143
+print(x[0])
+"""
