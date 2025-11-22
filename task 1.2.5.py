cf8b670bd84cacfe49179a173a8ddf58a06d9aed
TaurusK <ilyakazim2@gmail.com>
2025-11-22 11:07:12 +0300

    Даны два числа. Проверьте, что первые цифры этих чисел совпадают.

--- task 1.2.5.py
+++ task 1.2.5.py
@@ -2,17 +2,17 @@
 # Задача 1.2 №5 Даны два числа. Проверьте, что первые цифры этих чисел совпадают.
 
 
-number3 = str(input('введите число:'))
+number1 = str(input('введите число:'))
+number2 = str(input('введите число:'))
+
 '''добавить проверку строки на цифры и буквы'''
-x = number3[0].isdigit()
-
-if x:
-   digit1 = int(number3)
-   print(len(number3))
+x = number1[0].isdigit()
+y = number2[0].isdigit()
+digit1 = int(number1[0])
+digit2 = int(number2[0])
+if digit1 == digit2: # проверка что число равняеться числу
+   print('числа совпадают')
 else:
-    print('тут есть буквы')
+    print('первые числа не совпадают')
 
 
-"""if z:
-    digit3 = int(number3[])
-        print(len(digit3)"""
